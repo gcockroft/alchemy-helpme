49dcaf24ed8ee5b639b264e86243d48e733079de
Gareth Cockroft <gcockroft534@gmail.com>
2023-07-17 23:28:30 -0400

    Getting conversation list
    Not yet retrieving all channels as desired.

--- app.js
+++ app.js
@@ -1,35 +1,66 @@
 const { App } = require('@slack/bolt');
 
-// Initialize app with the bot token and signing secret.
+let channelStore = {};
+
 const app = new App({
-    token: process.env.SLACK_BOT_TOKEN,
-    signingSecret: process.env.SLACK_SIGNING_SECRET,
-    socketMode: true,
+    token: process.env.SLACK_BOT_TOKEN, 
     appToken: process.env.SLACK_APP_TOKEN,
-    port: process.env.PORT || 3000
+    signingSecret: process.env.signingSecret,
+    socketMode: true,
 });
-
-app.message('!help', async ({ message, client, say }) => {
-    client.chat.postMessage({
-        token: SLACK_BOT_TOKEN,
-        channel: ""
-    })
-    await say({
-        // Takes value that is an object containing blocks and text.
-        // blocks makes text the fallback for notifications and accessibility. 
-        text: `Hey there <@${message.user}!`
-    });
-});
-
-// app.action('button_click', async ({ body, ack, say }) => {
-//     // Acknowledge the action.
-//     await ack();
-//     await say(`<@${body.user.id}> clicked the button`);
-// });
 
 (async () => {
-    // Start app
-    await app.start(process.env.PORT || 3000);
-    console.log('Bolt app is running!');
+    await app.start();
+    console.log('⚡️ Bolt app started');
+    getChannels();
 })();
 
+// subscribe to 'app_mention' event in your App config
+// need app_mentions:read and chat:write scopes
+app.event('app_mention', async ({ event, context, client, say }) => {
+    console.log('mentioned');
+    try {
+        sendMessage(`Now I'm sending a message to <@${event.user}> in a different channel`,
+            'dev-batcall1',
+            'h',
+        )
+    } catch (error) {
+        console.error(error);
+    }
+});
+
+async function getChannels() {
+    try {
+        console.log('Getting channels');
+        // Uses API to get all non-archived channels. 
+        const result = await app.client.conversations.list({
+            token: process.env.SLACK_BOT_TOKEN,
+            type: 'public,private,mpim,im',
+            exclude_archived: true
+        });
+
+        // Store in mapping of channel name -> ID.
+        result.channels.forEach((channel) => {
+            const channelName = channel['name'];
+            const channelId = channel['id'];
+
+            channelStore[channelName] = channelId;
+        })
+    } catch (error) {
+        console.error(error);
+    }
+}
+
+// Function for sending any message to any user group in any specificied channel. 
+// Pass the channel name for readability.
+async function sendMessage(text, channelName, userGroup) {
+    const channelId = channelStore[channelName];
+    try {
+        const res = await app.client.chat.postMessage({
+            channel: channelId,
+            text: `${text}`
+        })
+    } catch (error) {
+        console.error(error);
+    }
+}
